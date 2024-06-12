eb2697b2b30cf7cb77145883734a59ac14fc0bc2
wengjq <wengjq@mingyuanyun.com>
2024-06-12 10:44:38 +0800

    feat: prettier

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,8 +1,8 @@
 import NextImage, { ImageProps } from 'next/image'
 
 const Image = ({ ...rest }: ImageProps) => {
-    const {src, ...res} = rest
-    return <NextImage {...res} src={`/Front-end-learning${src}`}/>
+  const { src, ...res } = rest
+  return <NextImage {...res} src={`/Front-end-learning${src}`} />
 }
 
 export default Image
