5b013df0021811730e7131cac3b5df963cfbf769
wengjq <wengjq@mingyuanyun.com>
2024-06-12 10:41:44 +0800

    feat: change basepath

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,5 +1,8 @@
 import NextImage, { ImageProps } from 'next/image'
 
-const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
+const Image = ({ ...rest }: ImageProps) => {
+    const {src, ...res} = rest
+    return <NextImage {...res} src={`/Front-end-learning${src}`}/>
+}
 
 export default Image
